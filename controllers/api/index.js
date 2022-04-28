75f9fc622efd8132f5ca37cba3a880e565ee7eac
Jordan <jordan.m.edginton@gmail.com>
2022-04-27 19:29:21 -0600

    added home-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 // Requirements for router to work and communicate with the server
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes.js');
 const tasksRoutes = require('./task-routes');
 // Declaration that requires the router to use these files and routes
 router.use('/user', userRoutes);
