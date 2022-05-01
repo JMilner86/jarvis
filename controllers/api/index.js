b5bb9238c1c127b3bd171484b61d4d5b5ea54564
Jordan <jordan.m.edginton@gmail.com>
2022-05-01 11:25:24 -0600

    moved routes to correct folders, tasks endpoint renders

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 // Requirements for router to work and communicate with the server
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
-const tasksRoutes = require('./task-routes');
+//const tasksRoutes = require('./task-routes');
 // Declaration that requires the router to use these files and routes
 router.use('/users', userRoutes);
-router.use('/tasks', tasksRoutes);
+//router.use('/tasks', tasksRoutes);
 
 
 module.exports = router;
---
+++ controllers/task-routes.js
@@ -0,0 +1,45 @@
+const { Tasks } = require('../models');
+//const User = require('../../models/User');
+const withAuth = require('../utils/auth');
+const router = require('express').Router();
+
+// Retrieves all the Tasks associated with the logged in user. (Thats what it's supposed to do)
+router.get('/', (req, res) => {
+  console.log('==============')
+    Tasks.findAll({
+      // where: {
+      //   id: req.params.User
+      // },
+      attributes: [
+      'id',
+      'title',
+      'task_info',
+      'task_timer'
+      ]
+    })
+    .then(dbTaskData => {
+      const tasks = dbTaskData.map(tasks => tasks.get({ plain: true }));
+      res.render('task', { tasks, loggedIn: true });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
+
+// router.post('/', withAuth, (req, res) => {
+//   Tasks.create({
+//     title: req.body.title,
+//     task_info: req.body.task_info,
+//     task_timer: req.session.task_timer
+//   })
+//     .then(dbTasksData => res.json(dbTasksData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+
+
+module.exports = router
